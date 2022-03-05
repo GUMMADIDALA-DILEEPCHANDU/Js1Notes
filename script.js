cf967bb4fb4a4140393ac41be4c9637bc3a53b19
GUMMADIDALA-DILEEPCHANDU <74855969+GUMMADIDALA-DILEEPCHANDU@users.noreply.github.com>
2022-03-05 10:27:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Script of image mouse event
+// External js
+//mouseover $ mouseout events
+function setNewImage(){
+ //  alert();
+   document.getElementById("img1").src="loader.gif"
+}
+function setOldImage(){
+     document.getElementById("img1").src="eye.png"
+}
